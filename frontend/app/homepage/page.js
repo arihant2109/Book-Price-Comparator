06a6878701629461dea5c115d1666973e28ce752
arihant-ttn <arihant.kulshrestha@tothenew.com>
2025-04-17 17:25:02 +0530

    commit

--- frontend/app/homepage/page.js
+++ frontend/app/homepage/page.js
@@ -4,7 +4,7 @@ import BookCard from '@/components/BookCard'
 const Page = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.searchSection}><input type='search' placeholder='     Search for a Book or Author.....'/></div>
+        <div className={styles.searchSection}><input type='search' placeholder='    Search for a Book or Author.....'/></div>
         <div><h1>Trending Books</h1>
         
       <BookCard/>
